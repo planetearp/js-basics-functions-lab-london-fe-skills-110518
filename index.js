104cd6aaf34f3ff8234c5cfe9a4cb7e87f3fa76f
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2018-12-05 20:06:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(dist) {
+  return Math.abs(dist - 42);
+}
+
+function distanceFromHqInFeet(dist) {
+  return (distanceFromHqInBlocks(dist) * 264);
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264;
+}
