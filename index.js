d1ce6b1c25fa6a7be2e713b523e0d9d6cbe2cd3d
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2018-12-05 20:34:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(dist) {
+  return Math.abs(dist - 42);
+}
+
+function distanceFromHqInFeet(dist) {
+  return (distanceFromHqInBlocks(dist) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = Math.abs(start - destination) * 264;
+  if (distance < 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance >= 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
