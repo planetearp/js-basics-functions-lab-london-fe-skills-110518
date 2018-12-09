3ffa4940dfde2376fe22ab78973e43557d1ac374
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2018-12-09 01:18:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function distanceTravelledInFeet(start, destination) {
 }
 
 function calculatesFarePrice(start, destination) {
-  let distance = Math.abs(start - destination) * 264;
+  // let distance = Math.abs(start - destination) * 264;
+  let distance = distanceTravelledInFeet(start, destination);
   if (distance < 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
